8195845dc97dd20912f1457c054e022acf410017
sasha <sasha.joukov@gmail.com>
2024-02-03 16:11:08 -0500

    Send request

--- dareflix-scripts.js
+++ dareflix-scripts.js
@@ -2,33 +2,40 @@ var dares_json;
 var lobbyId;
 var lobby;
 var lobbies;
+var usrIndex;
 
 function bodyOnload() {
+    // lobbyId = 0;
+    // enterLobby();
+    // return false;
 }
 
 
 function enterLobby() {
     lobbyId = document.getElementById("lobbyId").value;
-    if (lobbies != null && lobbies[lobbyId] != null) {
-        lobby = lobbies[lobbyId];
-        let usrIndex = 0;
-        for (; usrIndex < 1000; usrIndex++) {
-            if (lobby[usrIndex] == null) {
-                break;
-            }
-        }
-        if (usrIndex == 1000) {
-            // console.log("Couldn't find usrIndex");
-        } else { // success
-            lobby[usrIndex] = {
-                "dares":[],
-                "points":"0"
-            }
-            // console.log("Found usrIndex");
-            document.getElementById("lobby").style.display = "none";
-            document.getElementById("dares").style.display = "block";
+    if (lobbies == null || lobbies[lobbyId] == null) {
+        invalidLobby();
+        return false;
+    }
+
+    lobby = lobbies[lobbyId];
+    usrIndex = 0;
+    for (; usrIndex < 1000; usrIndex++) {
+        if (lobby[usrIndex] == null) {
+            break;
         }
     }
+    if (usrIndex == 1000) {
+        // console.log("Couldn't find usrIndex");
+    } else { // success
+        lobby[usrIndex] = {
+            "dares":[],
+            "points":"0"
+        }
+        // console.log("Found usrIndex");
+        switchToDares();
+    }
+    return false;
 }
 function submitText() {
     let playerId = document.getElementById("playerId").value;
@@ -43,4 +50,48 @@ function submitText() {
     // Simulate sending data to a server or writing to a file
     console.log("JSON Data:", jsonString);
 }
-//function 
+function switchToDares() {
+    document.getElementById("lobby").style.display = "none";
+    genDares();
+}
+
+function genDares() {
+    document.getElementById("dares").style.display = "block";
+    addDareLine();
+}
+function addDareLine() {
+    let daresDiv = document.getElementById("daresDiv");
+    let dareLine = daresDiv.appendChild(document.createElement("div"));
+    dareLine.className = "dareLine";
+    dareText = dareLine.appendChild(document.createElement("input"));
+    dareText.type = "text";
+    dareText.className = "dareText";
+    dareText.placeholder = "Enter dare here";
+    dareAdd = dareLine.appendChild(document.createElement("button"));
+    dareAdd.className = "dareAdd";
+    dareAdd.innerText = "Add";
+    let index = daresDiv.children.length;
+    dareAdd.onclick = function () {addDare(this, index);};
+}
+
+function addDare(addButton, index) {
+    lobby[usrIndex]["dares"][index] = addButton.parentNode.children[0].value;
+    addButton.innerText = "Update";
+}
+
+function submitDares() {
+    let xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new activeXObject("Microsoft.XMLHTTP");
+    xhr.onstatechange = function () {
+        alert("DONE");
+    }
+    xhr.open('POST', '/json_handler', true);
+    xhr.send(lobbies);
+}
+
+function invalidLobby() {
+    document.getElementById("invalidLobby").style.display="inline";
+}
+
+// function enterDare() {
+
+// }
